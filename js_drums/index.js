253b610e6328da8e736058624dad1bd478e75473
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-06 13:10:21 -0500

    Got stuck because I had chrome giving me issues when I was trying to play the audion in a function that was being passed to an event listener, so had to watch a bit of the videos to see how the instructor did it. Watched just enough to see some of her javascript, so what I implemented is a blend of my code and hers. Not very pretty though.

---
+++ js_drums/index.js
@@ -0,0 +1,42 @@
+var numDrums = document.querySelectorAll(".drum").length;
+
+document.addEventListener("keydown", function (e) {
+  playSound(e.code[3].toLowerCase());
+});
+
+for (var i = 0; i < numDrums; ++i){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    playSound(this.innerHTML);
+  });
+}
+
+function playSound(letter){
+  var audio;
+  switch (letter) {
+    case 'w':
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case 'a':
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case 's':
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case 'd':
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case 'j':
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case 'k':
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case 'l':
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    default:
+      console.log(letter);
+      return;
+  }
+  audio.play();
+}
