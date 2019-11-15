30df718c1cebd52fbc51149dbc2c2f3637703866
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-15 12:43:29 -0500

    Adding base files for the newsletter signup website.

---
+++ api_newsletter/app.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
