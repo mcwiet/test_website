94d2c96effef739102f1a901f78f9c5f9a9b42f6
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-20 09:55:32 -0500

    Work for the newsletter signup page is complete. Committing to setup for heroku deployment.

--- api_newsletter/app.js
+++ api_newsletter/app.js
@@ -6,8 +6,62 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.post("/", function(req, res) {
+  var first = req.body.firstName;
+  var last = req.body.lastName;
+  var email = req.body.email;
+
+  var data = {
+    email_address: email,
+    status: "subscribed",
+    "merge_fields": {
+      "FNAME": first,
+      "LNAME": last,
+    },
+  };
+
+  var json_data = JSON.stringify(data);
+
+  var options = {
+    url: "https://us5.api.mailchimp.com/3.0/lists/f12eb6f870/members/",
+    method: "POST",
+    headers: {
+      "Authorization": "m.cwiet@vt.edu e0722ad1eaf12b347cc0fd924a6272a2-us5",
+    },
+    body: json_data
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000");
 });
+
+
+
+// e0722ad1eaf12b347cc0fd924a6272a2-us5
+// f12eb6f870
